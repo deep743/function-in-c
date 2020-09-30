2371fc3bae5e070309fec090c841dd69e2bbaabf
Deep Choudhury <72141495+deep743@users.noreply.github.com>
2020-09-30 23:57:49 +0530

    function file uploded

---
+++ Function4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int sum(int n[])
+{
+    int s=0,i
+    for(i=0 i<10; i++
+    {
+    
+        s=s+n[i]
+    }
+    return s;
+}
+
+int main()
+{
+    int i,a[10],s=0
+    print("Enter 10 numbers :- \n"
+    
+    for(i=0; i<10; i++)
+    {
+        scanf("%d",&a[i]
+    }
+    print(" the sum of the 10 number is = %d",sum(a
+}
